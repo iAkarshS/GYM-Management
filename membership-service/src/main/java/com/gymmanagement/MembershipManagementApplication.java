8c22134b25180a0488a315f49b10f7d0968ce841
iAkarshS <iAkarshS@users.noreply.github.com>
2023-04-29 22:35:15 +0100

    first

---
+++ membership-service/src/main/java/com/gymmanagement/MembershipManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gymmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipManagementApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipManagementApplication.class, args);
+	}
+
+}
---
+++ trainer-service/src/main/java/com/gymmanagement/TrainerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gymmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerManagementApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerManagementApplication.class, args);
+	}
+
+}
